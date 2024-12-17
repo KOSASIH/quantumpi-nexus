5b30b391381673797be0692fff11eb9202198273
KOSASIH <kosasihg88@gmail.com>
2024-12-17 14:08:07 +0700

    Create __init__.py

---
+++ src/main/ai_integration/__init__.py
@@ -0,0 +1,6 @@
+# src/main/ai_integration/__init__.py
+
+from .market_analysis import MarketAnalysis
+from .predictive_insights import PredictiveInsights
+
+__all__ = ['MarketAnalysis', 'PredictiveInsights']

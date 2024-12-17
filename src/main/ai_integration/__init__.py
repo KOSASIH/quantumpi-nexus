872ef3efbcaf7f265952709155edbaa0fa5185c3
KOSASIH <kosasihg88@gmail.com>
2024-12-17 14:15:56 +0700

    Update __init__.py

--- src/main/ai_integration/__init__.py
+++ src/main/ai_integration/__init__.py
@@ -1,6 +1,1 @@
-# src/main/ai_integration/__init__.py
 
-from .market_analysis import MarketAnalysis
-from .predictive_insights import PredictiveInsights
-
-__all__ = ['MarketAnalysis', 'PredictiveInsights']

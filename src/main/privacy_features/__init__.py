90ec436d684235a019065b8acd90a8686159fba0
KOSASIH <kosasihg88@gmail.com>
2024-12-17 14:16:59 +0700

    Update __init__.py

--- src/main/privacy_features/__init__.py
+++ src/main/privacy_features/__init__.py
@@ -1,6 +1,1 @@
-# src/main/privacy/__init__.py
 
-from .zero_knowledge_proofs import ZeroKnowledgeProof
-from .privacy_enhanced_transactions import PrivacyEnhancedTransaction
-
-__all__ = ['ZeroKnowledgeProof', 'PrivacyEnhancedTransaction']
